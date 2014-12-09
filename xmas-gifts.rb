8a1693e8e8a98ceca8240c3b31abd5863028e980
Andy <andygnewman@gmail.com>
2014-12-09 09:20:04 +0000

    working gift log

--- xmas-gifts.rb
+++ xmas-gifts.rb
@@ -1,12 +1,12 @@
 require 'csv'
-@students = []
+@xmasgifts = []
 
-def try_load_students
+def try_load_gifts
 	filename = ARGV.first # first argument passed from the command line
 	return if filename.nil? # exits out of method if no argument passed
 	if File.exists?(filename)
 		load_students(filename)
-		puts "Loaded #{@students.length} records from #{filename}"
+		puts "Loaded #{@xmasgifts.length} records from #{filename}"
 	else
 		puts "Sorry #{filename} doesn't exist."
 		exit
@@ -21,112 +21,112 @@ def interactive_menu
 end
 
 def print_menu
-	puts "1. Input the students"
-	puts "2. Show the students"
-	puts "3. Save students to csv file"
-	puts "4. Load students from csv file"
+	puts "1. Input gifts"
+	puts "2. Show the gifts"
+	puts "3. Save gifts to csv file"
+	puts "4. Load gifts from csv file"
 	puts "9. Exit"
 end
 
 def process(selection)
 	case selection
-		when "1" then input_students 
-		when "2" then show_students 
-		when "3" then save_students 
-		when "4" then load_students 
+		when "1" then input_gifts 
+		when "2" then show_gifts 
+		when "3" then save_gifts 
+		when "4" then load_gifts 
 		when "9" then exit
 		else puts "I don't know what you mean, please try again."		
 	end
 end
 
-def input_students
-	puts "Please enter the names of the students"
+def input_gifts
+	puts "Please enter a description of the gift received"
 	puts "To finish, just hit return twice!"
-	capture_name
-	while !@name.empty? do
-		add_students(@name, capture_cohort, capture_country)
-		print "You have entered #{@students.length} student name"
-		puts @students.length != 1 ? "s" : ""
-		capture_name
+	capture_gift
+	while !@gift.empty? do
+		add_gifts(@gift, capture_receiver, capture_giver)
+		print "You have entered #{@xmasgifts.length} gift"
+		puts @xmasgifts.length != 1 ? "s" : ""
+		capture_gift
 	end
-	@students
+	@xmasgifts
 end 
 
-def capture_name
-	@name = STDIN.gets.chomp
+def capture_gift
+	@gift = STDIN.gets.chomp
 end
 
-def capture_cohort
-	puts "Please enter the cohort, or return to use december default"
-	cohort = STDIN.gets.chomp
-	if cohort.empty?
-		cohort = 'december'
+def capture_receiver
+	puts "Please enter the receiver of the gift, or return to use unknown as default"
+	receiver = STDIN.gets.chomp
+	if receiver.empty?
+		receiver = 'UK'
 	end
-	cohort
+	receiver
 end
 
-def capture_country
-	puts "Please enter the student's country, or return to use UK default"
-	country = STDIN.gets.chomp
-	if country.empty?
-		country = 'UK'
+def capture_giver
+	puts "Please enter who gave the gift, or return to use unknown as default"
+	giver = STDIN.gets.chomp
+	if giver.empty?
+		giver = 'unknown'
 	end
-	country
+	giver
 end
 
-def add_students(name, cohort, country)
-	@students << {:name => @name, :cohort => cohort.to_sym, :country => country}
+def add_gifts(gift, receiver, giver)
+	@xmasgifts << {:gift => @gift, :receiver => receiver, :giver => giver}
 end
 
-def show_students
-	if !@students.empty?
+def show_gifts
+	if !@xmasgifts.empty?
 		print_header
-		print_students_list
+		print_gift_list
 		print_footer
 	else
-		puts "You don't have any student names"
+		puts "You don't have any gifts"
 	end
 end
 
 def print_header
-puts "The students of my cohort at Makers Academy"
-puts "-------------------------------------------"	
+puts "The gifts of Christmas 2014 at 32 Temple Road"
+puts "---------------------------------------------"	
 end
 
-def print_students_list
-	@students.sort_by{ |x| x[:cohort]}.each.with_index(1) do |student, index|
+def print_gift_list
+	@xmasgifts.sort_by{ |x| x[:receiver]}.each do |gifts|
 		# optional filters
 		# if student[:name][0].downcase == 'a'
 		# if student[:name].length < 12
-	puts "#{index}. #{student[:name].ljust(20)}, #{student[:cohort]} cohort, from #{student[:country]}"
+	puts "#{gifts[:receiver]} received #{gifts[:gift]} from #{gifts[:giver]}"
 	end	
 end
 
 def print_footer 
-puts "Overall we have #{@students.length} great students"
+puts "Overall we have received #{@xmasgifts.length} fabulous presents"
 end
 
-def load_students # got stuck on trying to parse each line read in, until realised it was read as an array!
+def load_gifts # got stuck on trying to parse each line read in, until realised it was read as an array!
 	CSV.foreach(get_filename) do |line| # doesn't validate that user input filename exists
-		@name = line[0]
-		cohort = line[1]
-		country = line[2]
-		add_students(@name, cohort, country)
+		@gift = line[0]
+		receiver = line[1]
+		giver = line[2]
+		add_gifts(@gift, receiver, giver)
 	end
 end
 
-def save_students
+def save_gifts
 	CSV.open(get_filename, "wb") do |csv|
-		@students.each do |student|
-  		csv << [student[:name], student[:cohort], student[:country]]
+		@xmasgifts.each do |gifts|
+  		csv << [gifts[:gift], gifts[:receiver], gifts[:giver]]
 	end
 end
 end
 
 def get_filename
-	filename = "students.csv" # sets default filename can be overwritten by user
+	filename = "xmasgifts.csv" # sets default filename can be overwritten by user
 	puts "What filename would you like to use"
-	puts "just hit enter to use default of students.csv"
+	puts "just hit enter to use default of xmasgifts.csv"
 	user_filename = STDIN.gets.chomp
 	if !user_filename.empty? # if user has input something use that file name rather than the default
 		filename = user_filename
@@ -134,5 +134,5 @@ def get_filename
 	filename
 end
 
-try_load_students
+try_load_gifts
 interactive_menu
